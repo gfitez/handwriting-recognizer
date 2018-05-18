449fb5f5341b83ea0a320f5b20d14e88c9b2b24d
Grant Fitez <grantfitez@gmail.com>
2018-05-17 23:01:28 -0600

    Getting data and image extraction

--- hostServer.js
+++ hostServer.js
@@ -1,7 +1,26 @@
+var express = require('express');
+var app = express();
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
+require("jquery")
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html.html'));
+});
+
+
+app.get('/getData', function(req, res){
+  res.type('pdf');
+  res.sendFile(path.join(__dirname + '/data'+req.query.d),"binary")
+});
+
+app.get('/letters.js', function(req, res){
+  res.type('pdf');
+  res.sendFile(path.join(__dirname + '/letters.js'),"binary")
+});
+
+app.listen(3000, '0.0.0.0', function() {//connect on other computers using network name/ip
+    console.log('Listening to port:  ' + 3000);
+});
---
+++ letters.js
@@ -0,0 +1,68 @@
+var letters=[];
+function requestLetterData(i){
+  var oReq = new XMLHttpRequest();
+  oReq.open("GET", "/getData?d="+i, true);
+  oReq.responseType = "arraybuffer";
+
+  oReq.onload = function(oEvent) {
+    var arrayBuffer = oReq.response;
+
+    // if you want to access the bytes:
+    letters[i]= new Uint8Array(arrayBuffer);
+    console.log(i)
+    if(i<9){requestLetterData(i+1)}else{
+      console.log(1)
+    };
+  },
+  oReq.send();
+}
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function getNthLetter(letter,index){
+  var temp=[];
+  for(var i=0;i<28;i++){temp.push([])}
+  for(var i=0;i<28;i++){
+    temp.push([])
+    for(var j=28-1;j>=0;j--){
+      temp[i][j]=(letters[letter][i*28+index*28*28+j]);
+    }
+  }
+
+  var arr=[]
+  for(var i=0;i<28;i++){arr.push([])}
+  for(var i=0;i<28;i++){
+
+    for(var j=0;j<28;j++){
+      arr[i][j]=temp[27-j][i] ;
+    }
+  }
+
+  for(var i=0;i<28;i++){
+    for(var j=0;j<14;j++){
+      temp1=arr[i][j];
+      arr[i][j]=arr[i][28-j-1];
+      arr[i][28-j-1]=temp1;
+    }
+  }
+
+  return arr;
+}
+
+function drawArray(arr){
+
+  for(var i=0;i<28;i+=1){
+    for(var j=0;j<28;j++){
+
+      ctx.fillStyle=rgbToHex(arr[i][j],0,0);
+
+      ctx.fillRect(i*5,j*5,5,5 )
+    }
+  }
+}
